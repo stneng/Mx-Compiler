18df22ffbbf94ecc006dc629e79b572eb63ebbdc
stneng <git@stneng.com>
2021-01-15 22:06:10 +0800

    update bash

--- build.bash
+++ build.bash
@@ -1,4 +1,4 @@
 set ff=UNIX
 set -e
-mkdir out
-find ./src -name *.java | javac -d out -cp /ulib/java/antlr-4.9.1-complete.jar @/dev/stdin
+mkdir -p bin
+find ./src -name *.java | javac -d bin -cp /ulib/java/antlr-4.9.1-complete.jar @/dev/stdin
